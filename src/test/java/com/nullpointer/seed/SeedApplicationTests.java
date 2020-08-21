7bf6bae2ef2454e7a86250f3fdfa30b181ed497d
nullpointer <lxy360@vip.qq.com>
2020-08-21 23:54:36 +0800

    feat: init seed project

---
+++ src/test/java/com/nullpointer/seed/SeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullpointer.seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
