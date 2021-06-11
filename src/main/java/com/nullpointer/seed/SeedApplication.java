b1d480d32c2f6a369a0f7d6b00e00d306a3d7140
lihongzheng <hongzheng.li@thoughtworks.com>
2021-06-11 10:17:09 +0800

    chore: add author

--- src/main/java/com/nullpointer/seed/SeedApplication.java
+++ src/main/java/com/nullpointer/seed/SeedApplication.java
@@ -3,6 +3,9 @@ package com.nullpointer.seed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lihongzheng
+ */
 @SpringBootApplication
 public class SeedApplication {
 
