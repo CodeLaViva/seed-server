ab2488fdc19314b3d714bc251d6d7ed33a3b4967
lihongzheng <hongzheng.li@thoughtworks.com>
2021-06-16 15:40:37 +0800

    feat: add setting sample

--- src/main/java/com/nullpointer/seed/controllers/SampleController.java
+++ src/main/java/com/nullpointer/seed/controllers/SampleController.java
@@ -1,6 +1,9 @@
 package com.nullpointer.seed.controllers;
 
+import com.nullpointer.seed.configs.Setting;
 import com.nullpointer.seed.models.Sample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +15,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController("/sample")
 public class SampleController {
 
-    public SampleController() {}
+    private final Setting setting;
+
+    @Value("${app.environment}")
+    private String environment;
+
+    @Autowired
+    public SampleController(Setting setting) {
+        this.setting = setting;
+    }
 
     @GetMapping("{id}")
     public ResponseEntity<String> get(@PathVariable String id) {
-        return ResponseEntity.ok(String.format("Request Id is %s", id));
+        return ResponseEntity.ok(String.format("Request Id: %s, current environment: %s， %s", id, setting.getEnvironment(), environment));
     }
 
     @DeleteMapping("{id}")
