b39918b1cae13cb17b29eb4ae65d0388f688b917
lihongzheng <hongzheng.li@thoughtworks.com>
2021-06-16 15:40:11 +0800

    fix： ignore DataSourceConfiguration

--- src/main/java/com/nullpointer/seed/SeedApplication.java
+++ src/main/java/com/nullpointer/seed/SeedApplication.java
@@ -2,11 +2,12 @@ package com.nullpointer.seed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author lihongzheng
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class SeedApplication {
 
     public static void main(String[] args) {
