701d25e569add49550057926e2df186f8dc6cfdd
lihongzheng <hongzheng.li@thoughtworks.com>
2021-07-15 16:15:07 +0800

    feat: add SampleServiceTest

---
+++ src/test/java/com/nullpointer/seed/services/SampleServiceTest.java
@@ -0,0 +1,15 @@
+package com.nullpointer.seed.services;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author lihongzheng
+ * @date 2021/6/29
+ * @description
+ */
+public class SampleServiceTest {
+
+    @Test
+    public void test() {
+    }
+}
