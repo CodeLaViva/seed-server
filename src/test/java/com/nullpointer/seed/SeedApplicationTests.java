3b0596f43ed2fcdae35eaf001d2250b04fe8228e
nullpointer <lxy360@vip.qq.com>
2022-05-27 00:12:38 +0800

    [hongzheng] temporarily comment UnitTest.

--- src/test/java/com/nullpointer/seed/SeedApplicationTests.java
+++ src/test/java/com/nullpointer/seed/SeedApplicationTests.java
@@ -1,9 +1,11 @@
 package com.nullpointer.seed;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//
+//@SpringBootTest
+//@DataJpaTest
 class SeedApplicationTests {
 
     @Test
