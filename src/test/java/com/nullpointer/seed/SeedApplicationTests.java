8a470f672494c01df476d16f1371f74c922866bb
nullpointer <lxy360@vip.qq.com>
2023-12-19 16:42:40 +0800

    [hongzheng] chore: adjust config.

--- src/test/java/com/nullpointer/seed/SeedApplicationTests.java
+++ src/test/java/com/nullpointer/seed/SeedApplicationTests.java
@@ -3,8 +3,8 @@ package com.nullpointer.seed;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
+
+@SpringBootTest
 //@DataJpaTest
 class SeedApplicationTests {
 
