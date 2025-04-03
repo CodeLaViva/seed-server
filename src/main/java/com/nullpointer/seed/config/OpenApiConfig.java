5c0ba41f2580b5d22ba54877b7092f8fd56494e8
nullpointer <lxy360@vip.qq.com>
2025-04-03 15:33:15 +0800

    [hongzheng] feat: add open api config.

---
+++ src/main/java/com/nullpointer/seed/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.nullpointer.seed.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Seed Server API")
+                        .version("1.0")
+                        .description("Seed Server API Documentation"));
+    }
+}
