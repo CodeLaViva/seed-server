098ddfb92ae5dd179a639a64081290f91e8c39a6
nullpointer <lxy360@vip.qq.com>
2025-04-03 15:31:16 +0800

    [hongzheng] feat: add kafka configuration.

---
+++ src/main/java/com/nullpointer/seed/configs/KafkaProperties.java
@@ -0,0 +1,34 @@
+package com.nullpointer.seed.configs;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka")
+public class KafkaProperties {
+    private ProducerConfig producer;
+    private ConsumerConfig consumer;
+    
+    @Data
+    public static class ProducerConfig {
+        private int retries;
+        private int batchSize;
+        private int bufferMemory;
+        private String keySerializer;
+        private String valueSerializer;
+        private String acks;
+    }
+    
+    @Data
+    public static class ConsumerConfig {
+        private String groupId;
+        private boolean enableAutoCommit;
+        private int autoCommitInterval;
+        private String keyDeserializer;
+        private String valueDeserializer;
+        private int fetchMaxWait;
+        private int maxPollRecords;
+    }
+}
