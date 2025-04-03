f6d19e4ddcc6be36575ffefcefd391675078eb42
nullpointer <lxy360@vip.qq.com>
2025-04-03 15:35:03 +0800

    [hongzheng] feat: add feign client.

---
+++ src/main/java/com/nullpointer/seed/clients/SampleClient.java
@@ -0,0 +1,20 @@
+package com.nullpointer.seed.clients;
+
+import com.nullpointer.seed.dto.request.SampleRequest;
+import com.nullpointer.seed.dto.response.SampleResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "sample-service", url = "${sample.service.url:http://localhost:6666}")
+public interface SampleClient {
+    
+    @GetMapping("/sample/{id}")
+    ResponseEntity<SampleResponse> getSample(@PathVariable("id") Integer id);
+    
+    @PostMapping("/sample")
+    ResponseEntity<SampleResponse> createSample(@RequestBody SampleRequest request);
+    
+    @DeleteMapping("/sample/{id}")
+    ResponseEntity<Boolean> deleteSample(@PathVariable("id") Integer id);
+}
