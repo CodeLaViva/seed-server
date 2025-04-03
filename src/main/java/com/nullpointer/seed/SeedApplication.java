6c88c2518393d9755222f4a919ebf032ef9fcf33
nullpointer <lxy360@vip.qq.com>
2025-04-03 15:30:54 +0800

    [hongzheng] feat: add feign client configuration.

--- src/main/java/com/nullpointer/seed/SeedApplication.java
+++ src/main/java/com/nullpointer/seed/SeedApplication.java
@@ -2,6 +2,7 @@ package com.nullpointer.seed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableFeignClients
 public class SeedApplication {
 
     public static void main(String[] args) {
